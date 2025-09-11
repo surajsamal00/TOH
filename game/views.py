75fe9a22e85661637a19a8f4d92aa9da7d4637ba
HellBlaze <surajsamal00@gmail.com>
2025-09-11 19:35:26 +0530

    final_sept_v5

--- game/views.py
+++ game/views.py
@@ -15,7 +15,7 @@ def game_view(request):
     # Decrease by 1 use
     settings.available_uses -= 1
 
-    if settings.available_uses == 9:
+    if settings.available_uses == 6:
         settings.save()
         return redirect('rizz_master')
 
@@ -29,7 +29,7 @@ def rizz_master(request):
     and plays with sound when user clicks the play button.
     """
     settings = GameSettings.objects.first()
-    if not settings or settings.available_uses < 8 :
+    if not settings or settings.available_uses < 5 :
         return redirect('no_uses_left')
     
     settings.available_uses -= 1
